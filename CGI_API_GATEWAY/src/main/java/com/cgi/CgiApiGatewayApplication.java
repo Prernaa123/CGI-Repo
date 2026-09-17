d5d08f65fb64b85760f4bf78241c33ef05230339
Prernaa123 <prernak044@gmial.com>
2026-09-17 18:02:41 +0530

    added apigatewayincomplete

---
+++ CGI_API_GATEWAY/src/main/java/com/cgi/CgiApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.cgi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgiApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgiApiGatewayApplication.class, args);
+	}
+
+}
